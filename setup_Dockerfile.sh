669c01c8bb093f515bf2bcfc125f4107b6754258
Noah Everett <noaheverett898@gmail.com>
2024-05-29 14:53:45 -0500

    added G4 to PATH variable

--- setup_Dockerfile.sh
+++ setup_Dockerfile.sh
@@ -33,6 +33,7 @@ export PATH="${PATH}:/ROOT_install/bin"
 # Geant4
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/Geant4_install/lib64"
 export Geant4_DIR="/Geant4_install/lib/cmake/Geant4"
+export PATH="${PATH}:/Geant4_install/bin"
 
 # GENIE
 export GENIE="/GENIE"
